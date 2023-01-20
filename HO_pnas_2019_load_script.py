6a2e5f2f7bfb04424349350a13b071bfd2a1a2ec
Gregory <setantaontherock1@gmail.com>
2023-01-20 15:15:03 -0500

    Initial commit with additional property defs and two ingest scripts and notebooks

---
+++ HO_pnas_2019_load_script.py
@@ -0,0 +1,97 @@
+from colabfit.tools.database import MongoDatabase, load_data
+from colabfit.tools.property_definitions import (
+    potential_energy_pd,
+    atomic_forces_pd,
+)
+import ase
+
+if "__name__" == "__main__":
+    client = MongoDatabase("test", drop_database=True)
+
+    def reader(file_path):
+        file_name = file_path.stem
+        atoms = ase.io.read(file_path, index=":")
+        for atom in atoms:
+            atom.info["name"] = file_name
+        return atoms
+
+    configurations = load_data(
+        # Data can be downloaded here:
+        # https://archive.materialscloud.org/record/2018.0020/v1
+        file_path="/Users/piper/Code/colabfit/data/liquid_solid_water/",
+        file_format="folder",
+        name_field="name",
+        elements=["H", "O"],
+        reader=reader,
+        glob_string="*.xyz",
+        generator=False,
+    )
+    # Load from colabfit's definitions
+    client.insert_property_definition(potential_energy_pd)
+    client.insert_property_definition(atomic_forces_pd)
+    metadata = {
+        "software": {"value": ["LAMMPS", "i-PI"]},
+        "method": {"value": ["revPBE0-D3", "DFT"]},
+    }
+    property_map = {
+        "potential-energy": [
+            {
+                "energy": {"field": "TotEnergy", "units": "eV"},
+                "per-atom": {"value": False, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+        "atomic-forces": [
+            {
+                "forces": {"field": "force", "units": "eV/Ang"},
+                "_metadata": metadata,
+            }
+        ],
+    }
+    ids = list(
+        client.insert_data(
+            configurations,
+            property_map=property_map,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+    hashes = client.get_data("configurations", fields=["hash"])
+    name = "HO_pnas_2019"
+    cs_ids = []
+    co_ids = client.get_data(
+        "configurations",
+        fields="hash",
+        query={"hash": {"$in": hashes}},
+        ravel=True,
+    ).tolist()
+
+    print(
+        "Configuration set ",
+        f"({name}):".rjust(22),
+        f"{len(co_ids)}".rjust(7),
+    )
+
+    cs_id = client.insert_configuration_set(
+        co_ids,
+        description="Liquid and solid H2O/water thermodynamics",
+        name=name,
+    )
+
+    cs_ids.append(cs_id)
+    ds_id = client.insert_dataset(
+        cs_ids,
+        all_do_ids,
+        name="HO_pnas_2019",
+        authors=["B. Cheng, E. Engel, J. Behler, C. Dellago, M. Ceriotti"],
+        links=[
+            "https://archive.materialscloud.org/record/2018.0020/v1",
+            "https://www.pnas.org/doi/full/10.1073/pnas.1815117116",
+        ],
+        description="1590 configurations of H2O/water "
+        "with potential energy and forces calculated using "
+        "a hybrid approach, DFT and revPBE0-D3 ",
+        verbose=True,
+    )
---
+++ property_definitions_additional.py
@@ -0,0 +1,267 @@
+dipole_moment_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/dipole-moment",
+    "property-name": "dipole-moment",
+    "property-title": "Dipole moment of a molecule",
+    "property-description": "The vector sum of dipole moments for all bonds in a molecule.",
+    "dipole-moment": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Dipole moment of the molecule.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+lumo_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/lumo-energy",
+    "property-name": "lumo-energy",
+    "property-title": "Lowest unoccupied molecular orbital (LUMO)",
+    "property-description": "Energy of the lowest unoccupied molecular orbital (LUMO)",
+    "lumo-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Energy of the lowest unoccupied molecular orbital (LUMO).",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+homo_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/homo-energy",
+    "property-name": "homo-energy",
+    "property-title": "Highest occupied molecular orbital (HOMO)",
+    "property-description": "Energy of the highest occupied molecular orbital (HOMO)",
+    "homo-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Energy of the highest occupied molecular orbital (HOMO).",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+isotropic_polarizability_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/isotropic-polarizability",
+    "property-name": "isotropic-polarizability",
+    "property-title": "Isotropic polarizability",
+    "property-description": "Polarizability of a molecule",
+    "isotropic-polarizability": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Polarizability of a molecule.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+
+homo_lumo_gap_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/homo-lumo-gap",
+    "property-name": "homo-lumo-gap",
+    "property-title": "HOMO-LUMO gap",
+    "property-description": "Difference in energy between the highest occupied molecular orbit (HOMO) and the lowest unoccupied molecular orbital (LUMO)",
+    "homo-lumo-gap": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Difference in energy between the highest occupied molecular orbit (HOMO) and the lowest unoccupied molecular orbital (LUMO)",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+electronic_spatial_extent_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/electronic-spatial-extent",
+    "property-name": "electronic-spatial-extent",
+    "property-title": "Electronic spatial extent",
+    "property-description": "A measure of the size of a molecule, using electron density and distance from molecular center of mass",
+    "electronic-spatial-extent": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "A measure of the size of a molecule, using electron density and distance from molecular center of mass",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+zpve_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/zpve",
+    "property-name": "zpve",
+    "property-title": "Zero-point vibrational energy",
+    "property-description": "Calculated vibrational energy in a molecule at 0 degrees Kelvin",
+    "zpve": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Calculated vibrational energy in a molecule at 0 degrees Kelvin",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+
+internal_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/internal-energy",
+    "property-name": "internal-energy",
+    "property-title": "Internal energy",
+    "property-description": "Internal energy of a molecule at defined temperature",
+    "internal-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "Internal energy of a molecule",
+    },
+    "temperature": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": True,
+        "description": "Temperature for which internal energy was calculated.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+enthalpy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/enthalpy",
+    "property-name": "enthalpy",
+    "property-title": "Enthalpy",
+    "property-description": "The sum of internal energy of the molecular system",
+    "enthalpy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The enthalpy of the molecular system",
+    },
+    "temperature": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": True,
+        "description": "Temperature for which enthalpy was calculated.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
+
+free_energy_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/free-energy",
+    "property-name": "free-energy",
+    "property-title": "Free energy from a static calculation",
+    "property-description": "Free energy from a calculation of a static configuration. Energies must be specified to be per-atom or supercell. If a reference energy has been used, this must be specified as well.",
+    "energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The free energy of the system.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": True,
+        "description": 'If True, "energy" is the total energy of the system, and has NOT been divided by the number of atoms in the configuration.',
+    },
+    "temperature": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": True,
+        "description": "Temperature for which free energy was calculated.",
+    },
+    "reference-energy": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": 'If provided, then "energy" is the energy (either of the whole system, or per-atom) LESS the energy of a reference configuration (E = E_0 - E_reference). Note that "reference-energy" is just provided for documentation, and that "energy" should already have this value subtracted off. The reference energy must have the same units as "energy".',
+    },
+}
+
+heat_capacity_pd = {
+    "property-id": "tag:staff@noreply.colabfit.org,2022-05-30:property/heat-capacity",
+    "property-name": "heat-capacity",
+    "property-title": "Heat capacity",
+    "property-description": "Heat capacity of a molecule",
+    "heat-capacity": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": False,
+        "description": "The heat capacity of a molecule",
+    },
+    "temperature": {
+        "type": "float",
+        "has-unit": True,
+        "extent": [],
+        "required": True,
+        "description": "Temperature for which heat capacity was calculated.",
+    },
+    "per-atom": {
+        "type": "bool",
+        "has-unit": False,
+        "extent": [],
+        "required": False,
+        "description": ".",
+    },
+}
