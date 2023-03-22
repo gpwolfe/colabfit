9ed7545d10fdb56cfbccc8b2b1200a7bcac872c5
gpwolfe <setantaontherock1@gmail.com>
2023-03-22 09:50:25 -0400

    Add template for scripts

---
+++ template.py
@@ -0,0 +1,148 @@
+"""
+author:gpwolfe
+
+Data can be downloaded from:
+
+Download link:
+
+Change database name as appropriate
+
+Run: $ python3 <script_name>.py -i (or --ip) <database_ip>
+
+Properties
+----------
+
+Other properties added to metadata
+----------------------------------
+
+File notes
+----------
+
+"""
+from argparse import ArgumentParser
+from colabfit.tools.configuration import AtomicConfiguration
+from colabfit.tools.database import MongoDatabase, load_data
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    potential_energy_pd,
+)
+import numpy as np
+from pathlib import Path
+import re
+import sys
+
+DATASET_FP = Path("")
+DATASET = ""
+
+SOFTWARE = ""
+METHODS = ""
+LINKS = ["", ""]
+AUTHORS = ""
+DS_DESC = ""
+ELEMENTS = [""]
+GLOB_STR = ".*"
+
+RE = re.compile(r"")
+
+
+def reader(filepath):
+
+    return atoms
+
+
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    args = parser.parse_args(argv)
+    client = MongoDatabase("----", nprocs=4, uri=f"mongodb://{args.ip}:27017")
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB_STR,
+        generator=False,
+    )
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(potential_energy_pd)
+
+    metadata = {
+        "software": {"value": SOFTWARE},
+        "method": {"value": METHODS},
+        # "": {"field": ""}
+    }
+    property_map = {
+        "potential-energy": [
+            {
+                "energy": {"field": "energy", "units": "eV"},
+                "per-atom": {"value": False, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+        "atomic-forces": [
+            {
+                "forces": {"field": "forces", "units": "eV/A"},
+                "_metadata": metadata,
+            }
+        ],
+    }
+    ids = list(
+        client.insert_data(
+            configurations,
+            property_map=property_map,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+    cs_regexes = [
+        [
+            f"{DATASET}",
+            ".*",
+            f"All configurations from {DATASET} dataset",
+        ]
+    ]
+
+    cs_ids = []
+
+    for i, (name, regex, desc) in enumerate(cs_regexes):
+        co_ids = client.get_data(
+            "configurations",
+            fields="hash",
+            query={
+                "hash": {"$in": all_co_ids},
+                "names": {"$regex": regex},
+            },
+            ravel=True,
+        ).tolist()
+
+        print(
+            f"Configuration set {i}",
+            f"({name}):".rjust(22),
+            f"{len(co_ids)}".rjust(7),
+        )
+        if len(co_ids) > 0:
+            cs_id = client.insert_configuration_set(
+                co_ids, description=desc, name=name
+            )
+
+            cs_ids.append(cs_id)
+        else:
+            pass
+
+    client.insert_dataset(
+        pr_hashes=all_do_ids,
+        name=DATASET,
+        authors=AUTHORS,
+        links=LINKS,
+        description=DS_DESC,
+        verbose=True,
+        cs_ids=cs_ids,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
