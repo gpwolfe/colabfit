c133bd40cfdcf03aaf12ad87bb6c01a6e984f8c8
gpwolfe <setantaontherock1@gmail.com>
2023-04-10 16:44:18 -0400

    Add scripts for ag-pbe, au-pbe, ag-au-pbe, dpgen, DCGAT1,2,3, and NequIP datasets

---
+++ scripts2/dcgat123/dcgat123.py
@@ -0,0 +1,367 @@
+"""
+author:gpwolfe
+
+Data can be downloaded from:
+https://doi.org/10.24435/materialscloud:m7-50
+
+Change database name as appropriate
+
+Run: $ python3 <script_name>.py -i (or --ip) <database_ip>
+
+Properties
+----------
+potential energy
+forces
+stress
+
+Other properties added to metadata
+----------------------------------
+Many other properties added, similar to data from Materials Project
+database
+
+File notes
+----------
+Dataset size is too large to run on local machine--insufficient memory
+"""
+from argparse import ArgumentParser
+from colabfit.tools.configuration import AtomicConfiguration
+from colabfit.tools.database import MongoDatabase, load_data
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    cauchy_stress_pd,
+    potential_energy_pd,
+)
+import json
+from pathlib import Path
+import sys
+
+DATASET_FP = Path("data")
+DATASET = "DCGAT"
+
+SOFTWARE = "VASP"
+METHODS = "DFT-PBE"
+LINKS = [
+    "https://doi.org/10.24435/materialscloud:m7-50",
+    "https://doi.org/10.1002/adma.202210788",
+]
+AUTHORS = "J. Schmidt, N. Hoffmann, H.-C. Wang, P. Borlido, P.J.M.A. Carriço,\
+ T.F.T. Cerqueira, S. Botti, M.A.L. Marques"
+DS_DESC = "Approximately 2.3 million configurations from 3 datasets curate\
+ for the purpose of training a crystal graph attention network machine\
+ learning model. Includes structures gathered from the Materials Project and\
+ AFLOW databases, among other sources."
+ELEMENTS = [
+    "Ac",
+    "Ag",
+    "Al",
+    "Ar",
+    "As",
+    "Au",
+    "B",
+    "Ba",
+    "Be",
+    "Bi",
+    "Br",
+    "C",
+    "Ca",
+    "Cd",
+    "Ce",
+    "Cl",
+    "Co",
+    "Cr",
+    "Cs",
+    "Cu",
+    "Dy",
+    "Er",
+    "Eu",
+    "F",
+    "Fe",
+    "Ga",
+    "Gd",
+    "Ge",
+    "H",
+    "He",
+    "Hf",
+    "Hg",
+    "Ho",
+    "I",
+    "In",
+    "Ir",
+    "K",
+    "Kr",
+    "La",
+    "Li",
+    "Lu",
+    "Mg",
+    "Mn",
+    "Mo",
+    "N",
+    "Na",
+    "Nb",
+    "Nd",
+    "Ne",
+    "Ni",
+    "Np",
+    "O",
+    "Os",
+    "P",
+    "Pa",
+    "Pb",
+    "Pd",
+    "Pm",
+    "Pr",
+    "Pt",
+    "Pu",
+    "Rb",
+    "Re",
+    "Rh",
+    "Ru",
+    "S",
+    "Sb",
+    "Sc",
+    "Se",
+    "Si",
+    "Sm",
+    "Sn",
+    "Sr",
+    "Ta",
+    "Tb",
+    "Tc",
+    "Te",
+    "Th",
+    "Ti",
+    "Tl",
+    "Tm",
+    "U",
+    "V",
+    "W",
+    "Xe",
+    "Y",
+    "Yb",
+    "Zn",
+    "Zr",
+]
+
+GLOB_STR = "*.json"
+
+
+def reader(filepath):
+    name = filepath.stem
+    with open(filepath) as f:
+        data = json.loads(f.read())
+    configs = []
+    for i, entry in enumerate(data["entries"]):
+        cell = entry["structure"]["lattice"]["matrix"]
+        elements = []
+        occu = []
+        abc = []
+        positions = []
+        magmom = []
+        charge = []
+        forces = []
+        for site in entry["structure"]["sites"]:
+            elements.append(site["species"][0]["element"])
+            occu.append(site["species"][0]["occu"])
+            abc.append(site["abc"])
+            positions.append(site["xyz"])
+            magmom.append(site["properties"]["magmom"])
+            charge.append(site["properties"]["charge"])
+            forces.append(site["properties"]["forces"])
+
+        config = AtomicConfiguration(
+            positions=positions,
+            cell=cell,
+            symbols=elements,
+        )
+        config.info["occu"] = occu
+        config.info["abc"] = abc
+        config.info["magmom"] = magmom
+        config.info["charge"] = charge
+        config.info["forces"] = forces
+        config.info["elements"] = elements
+
+        config.info["energy"] = entry["energy"]
+        config.info["stress"] = [list(x) for x in (entry["data"]["stress"])]
+        config.info["correction"] = entry["correction"]
+        config.info["energy_adjustments"] = [
+            x["value"] for x in entry["energy_adjustments"]
+        ]
+        config.info["mat_id"] = entry["data"]["mat_id"]
+
+        config.info["prototype_id"] = entry["data"]["prototype_id"]
+        config.info["spg"] = entry["data"]["spg"]
+
+        config.info["energy_total"] = entry["data"]["energy_total"]
+        config.info["total_mag"] = entry["data"]["total_mag"]
+        config.info["band_gap_ind"] = entry["data"]["band_gap_ind"]
+        config.info["band_gap_dir"] = entry["data"]["band_gap_dir"]
+        config.info["dos_ef"] = entry["data"]["dos_ef"]
+        config.info["energy_corrected"] = entry["data"]["energy_corrected"]
+        config.info["e_above_hull"] = entry["data"]["e_above_hull"]
+        config.info["e_form"] = entry["data"]["e_form"]
+        config.info["e_phase_separation"] = entry["data"]["e_phase_separation"]
+        config.info["decomposition"] = entry["data"]["decomposition"]
+
+        config.info["charge"] = entry["structure"]["charge"]
+
+        config.info["lattice_a"] = entry["structure"]["lattice"]["a"]
+        config.info["lattice_b"] = entry["structure"]["lattice"]["b"]
+        config.info["lattice_c"] = entry["structure"]["lattice"]["c"]
+        config.info["lattice_alpha"] = entry["structure"]["lattice"]["alpha"]
+        config.info["lattice_beta"] = entry["structure"]["lattice"]["beta"]
+        config.info["lattice_gamma"] = entry["structure"]["lattice"]["gamma"]
+        config.info["lattice_volume"] = entry["structure"]["lattice"]["volume"]
+        config.info["name"] = f"{name}_{i}"
+
+        configs.append(config)
+    return configs
+
+
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    args = parser.parse_args(argv)
+    client = MongoDatabase("----", nprocs=4, uri=f"mongodb://{args.ip}:27017")
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB_STR,
+        generator=False,
+    )
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(potential_energy_pd)
+    client.insert_property_definition(cauchy_stress_pd)
+
+    metadata = {
+        "software": {"value": SOFTWARE},
+        "method": {"value": METHODS},
+        # "": {"field": ""}
+    }
+
+    keys_used = [
+        k
+        for k in [
+            "occu",
+            "abc",
+            "magmom",
+            "charge",
+            "elements",
+            "correction",
+            "energy_adjustments",
+            "mat_id",
+            "prototype_id",
+            "spg",
+            "energy_total",
+            "total_mag",
+            "band_gap_ind",
+            "band_gap_dir",
+            "dos_ef",
+            "energy_corrected",
+            "e_above_hull",
+            "e_form",
+            "e_phase_separation",
+            "decompositio",
+            "lattice_a",
+            "lattice_b",
+            "lattice_c",
+            "lattice_alpha",
+            "lattice_beta",
+            "lattice_gamma",
+            "lattice_volume",
+        ]
+    ]
+    metadata.update({k: {"field": k} for k in keys_used})
+    property_map = {
+        "potential-energy": [
+            {
+                "energy": {"field": "energy", "units": "eV"},
+                "per-atom": {"value": False, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+        "atomic-forces": [
+            {
+                "forces": {"field": "forces", "units": "eV/A"},
+                "_metadata": metadata,
+            }
+        ],
+        "cauchy-stress": [
+            {
+                "stress": {"field": "stress", "units": "eV"},
+                "volume-normalized": {"value": True, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+    }
+    ids = list(
+        client.insert_data(
+            configurations,
+            property_map=property_map,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+    cs_regexes = [
+        [
+            f"{DATASET}-DCGAT-1",
+            "dcgat_1_*",
+            f"All configurations from {DATASET} dataset",
+        ],
+        [
+            f"{DATASET}-DCGAT-2",
+            "dcgat_2_*",
+            f"All configurations from {DATASET} dataset",
+        ],
+        [
+            f"{DATASET}-DCGAT-3",
+            "dcgat_3_*",
+            f"All configurations from {DATASET} dataset",
+        ],
+    ]
+
+    cs_ids = []
+
+    for i, (name, regex, desc) in enumerate(cs_regexes):
+        co_ids = client.get_data(
+            "configurations",
+            fields="hash",
+            query={
+                "hash": {"$in": all_co_ids},
+                "names": {"$regex": regex},
+            },
+            ravel=True,
+        ).tolist()
+
+        print(
+            f"Configuration set {i}",
+            f"({name}):".rjust(22),
+            f"{len(co_ids)}".rjust(7),
+        )
+        if len(co_ids) > 0:
+            cs_id = client.insert_configuration_set(
+                co_ids, description=desc, name=name
+            )
+
+            cs_ids.append(cs_id)
+        else:
+            pass
+
+    client.insert_dataset(
+        pr_hashes=all_do_ids,
+        name=DATASET,
+        authors=AUTHORS,
+        links=LINKS,
+        description=DS_DESC,
+        verbose=True,
+        cs_ids=cs_ids,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ scripts2/nequip_nature_2022/nequip_nc_2022.py
@@ -0,0 +1,177 @@
+"""
+author:gpwolfe
+
+Data can be downloaded from:
+
+Download link:
+
+Change database name as appropriate
+
+Run: $ python3 <script_name>.py -i (or --ip) <database_ip>
+
+Properties
+----------
+potential energy
+forces
+
+Other properties added to metadata
+----------------------------------
+None
+
+File notes
+----------
+Other datasets for this publication were taken from already-imported datasets:
+MD17, rMD17, etc.
+"""
+from argparse import ArgumentParser
+from ase.io import read
+from colabfit.tools.database import MongoDatabase, load_data
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    potential_energy_pd,
+)
+from pathlib import Path
+import sys
+
+DATASET_FP = Path().cwd()
+DATASET = "NequIP-NC-2022"
+
+SOFT_METH = {
+    "lipo-quench": ("DFT-PBE", "VASP"),
+    "lips": ("DFT-PBE", "VASP"),
+    "fcu": ("DFT", "CP2K"),
+}
+
+LINKS = [
+    "https://doi.org/10.24435/materialscloud:s0-5n",
+    "https://doi.org/10.1038/s41467-022-29939-5",
+]
+AUTHORS = "S. Batzner, A. Musaelian, L. Sun, M. Geiger, J.P. Mailoa, \
+ M. Kornbluth, N. Molinari, T.E. Smidt, B. Kozinsky"
+DS_DESC = "Approximately 57,000 configurations from the evaluation datasets\
+ for NequIP graph neural network model for\
+ interatomic potentials. Trajectories have been taken from LIPS, LIPO glass\
+ melt-quench simulation, and formate decomposition on Cu datasets. "
+ELEMENTS = ["H", "C", "Cu", "Li", "P", "S", "O"]
+GLOB_STR = "*.xyz"
+
+
+def reader(filepath):
+    name = filepath.stem
+    atoms = read(filepath, index=":", format="extxyz")
+    for i, atom in enumerate(atoms):
+        atom.info["energy"] = atom.get_potential_energy()
+        atom.info["forces"] = atom.get_forces()
+        atom.info["name"] = f"{name}_{i}"
+        atom.info["method"] = SOFT_METH[name][0]
+        atom.info["software"] = SOFT_METH[name][1]
+
+    return atoms
+
+
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    args = parser.parse_args(argv)
+    client = MongoDatabase("----", nprocs=4, uri=f"mongodb://{args.ip}:27017")
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB_STR,
+        generator=False,
+    )
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(potential_energy_pd)
+
+    metadata = {
+        "software": {"field": "software"},
+        "method": {"field": "method"},
+        # "": {"field": ""}
+    }
+    property_map = {
+        "potential-energy": [
+            {
+                "energy": {"field": "energy", "units": "eV"},
+                "per-atom": {"value": False, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+        "atomic-forces": [
+            {
+                "forces": {"field": "forces", "units": "eV/A"},
+                "_metadata": metadata,
+            }
+        ],
+    }
+    ids = list(
+        client.insert_data(
+            configurations,
+            property_map=property_map,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+    cs_regexes = [
+        [
+            f"{DATASET}-LIPS",
+            "lips*",
+            f"Lithium Thiophosphate (Li6.75P3S11) configurations from {DATASET} dataset",
+        ],
+        [
+            f"{DATASET}-LIPO-quench",
+            "lipo-quench*",
+            f"Lithium Phosphate amorphous glass (Li4P2O7) configurations from {DATASET} dataset",
+        ],
+        [
+            f"{DATASET}-Cu-formate",
+            "fcu*",
+            f"Cu-formate configurations, Cu <110> undergoing dehydrogenation decomposition, from {DATASET} dataset",
+        ],
+    ]
+
+    cs_ids = []
+
+    for i, (name, regex, desc) in enumerate(cs_regexes):
+        co_ids = client.get_data(
+            "configurations",
+            fields="hash",
+            query={
+                "hash": {"$in": all_co_ids},
+                "names": {"$regex": regex},
+            },
+            ravel=True,
+        ).tolist()
+
+        print(
+            f"Configuration set {i}",
+            f"({name}):".rjust(22),
+            f"{len(co_ids)}".rjust(7),
+        )
+        if len(co_ids) > 0:
+            cs_id = client.insert_configuration_set(
+                co_ids, description=desc, name=name
+            )
+
+            cs_ids.append(cs_id)
+        else:
+            pass
+
+    client.insert_dataset(
+        pr_hashes=all_do_ids,
+        name=DATASET,
+        authors=AUTHORS,
+        links=LINKS,
+        description=DS_DESC,
+        verbose=True,
+        cs_ids=cs_ids,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
