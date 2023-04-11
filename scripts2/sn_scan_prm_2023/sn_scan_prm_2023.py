dbac79cba61b48ef65646ee1223aad97bd089c69
gpwolfe <setantaontherock1@gmail.com>
2023-04-11 11:58:49 -0400

    Add script for Sn-SCAN-PRM-2023

---
+++ scripts2/sn_scan_prm_2023/sn_scan_prm_2023.py
@@ -0,0 +1,186 @@
+"""
+author:gpwolfe
+
+Data can be downloaded from:
+https://www.aissquare.com/datasets/detail?pageType=datasets&name=Sn-SCAN
+
+Change database name as appropriate
+
+Run: $ python3 <script_name>.py -i (or --ip) <database_ip>
+
+Properties
+----------
+energy
+forces
+virial
+
+Other properties added to metadata
+----------------------------------
+
+File notes
+----------
+"""
+from argparse import ArgumentParser
+from colabfit.tools.configuration import AtomicConfiguration
+from colabfit.tools.database import MongoDatabase, load_data
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    cauchy_stress_pd,
+    potential_energy_pd,
+)
+import numpy as np
+from pathlib import Path
+import sys
+
+DATASET_FP = Path().cwd()
+DATASET = "Sn-SCAN-PRM-2023"
+
+SOFTWARE = "VASP, DP-GEN"
+METHODS = "DFT-SCAN"
+LINKS = [
+    "https://www.aissquare.com/datasets/detail?pageType=datasets&name=Sn-SCAN",
+    "https://doi.org/10.48550/arXiv.2209.05059",
+]
+AUTHORS = "T. Chen, F. Yuan, J. Liu, H. Geng, L. Zhang, H. Wang, M. Chen"
+DS_DESC = "Approximately 6,500 configurations of Sn, including Sn8, Sn16 and\
+ Sn32, used in developing a deep potential that predicts the phase diagram of\
+ Sn"
+ELEMENTS = ["Sn"]
+GLOB_STR = "box.npy"
+
+
+def assemble_props(filepath: Path):
+    props = {}
+    prop_paths = list(filepath.parent.glob("*.npy"))
+    a_types = []
+    elem_map = dict()
+    if "type_map.raw" in [x.name for x in (filepath.parents[1].glob("*.raw"))]:
+        with open(filepath.parents[1] / "type_map.raw", "r") as f:
+            types = [x.strip() for x in f.readlines()]
+            for i, t in enumerate(types):
+                elem_map[i] = t
+        with open(filepath.parents[1] / "type.raw", "r") as f:
+            a_types = f.readlines()
+            if len(a_types) == 1:
+                a_types = [int(x.strip()) for x in a_types[0].split()]
+            else:
+                a_types = [int(x.strip()) for x in a_types]
+    elif "type_map.raw" in [x.name for x in (filepath.parent.glob("*.raw"))]:
+        with open(filepath.parent / "type_map.raw", "r") as f:
+            types = [x.strip() for x in f.readlines()]
+            for i, t in enumerate(types):
+                elem_map[i] = t
+        with open(filepath.parent / "type.raw", "r") as f:
+            a_types = f.readlines()
+            if len(a_types) == 1:
+                a_types = [int(x.strip()) for x in a_types[0].split()]
+            else:
+                a_types = [int(x.strip()) for x in a_types]
+    else:
+        pass
+    symbols = list(map(lambda x: elem_map.get(x), a_types))
+    for p in prop_paths:
+        key = p.stem
+        props[key] = np.load(p)
+    num_configs = props["force"].shape[0]
+    num_atoms = props["force"].shape[1] // 3
+    props["forces"] = props["force"].reshape(num_configs, num_atoms, 3)
+    props["coord"] = props["coord"].reshape(num_configs, num_atoms, 3)
+    props["box"] = props["box"].reshape(num_configs, 3, 3)
+    virial = props.get("virial")
+    if virial is not None:
+        props["virial"] = virial.reshape(num_configs, 3, 3)
+    props["symbols"] = symbols
+    return props
+
+
+def reader(filepath):
+    props = assemble_props(filepath)
+    configs = [
+        AtomicConfiguration(
+            symbols=props["symbols"], positions=pos, cell=props["box"][i]
+        )
+        for i, pos in enumerate(props["coord"])
+    ]
+    energy = props.get("energy")
+    for i, c in enumerate(configs):
+        c.info["forces"] = props["forces"][i]
+        virial = props.get("virial")
+        if virial is not None:
+            c.info["virial"] = virial[i]
+        # if energy is not None:
+        c.info["energy"] = float(energy[i])
+
+        c.info["name"] = f"{filepath.parts[-3]}_{filepath.parts[-2]}_{i}"
+    return configs
+
+
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    args = parser.parse_args(argv)
+    client = MongoDatabase("----", nprocs=4, uri=f"mongodb://{args.ip}:27017")
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB_STR,
+        generator=False,
+    )
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(potential_energy_pd)
+    client.insert_property_definition(cauchy_stress_pd)
+
+    metadata = {
+        "software": {"value": SOFTWARE},
+        "method": {"value": METHODS},
+        # "": {"field": ""}
+    }
+    property_map = {
+        "potential-energy": [
+            {
+                "energy": {"field": "energy", "units": "eV"},
+                "per-atom": {"value": False, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+        "atomic-forces": [
+            {
+                "forces": {"field": "forces", "units": "eV/A"},
+                "_metadata": metadata,
+            }
+        ],
+        "cauchy-stress": [
+            {
+                "stress": {"field": "virial", "units": "eV"},
+                "volume-normalized": {"value": True, "units": None},
+                "_metadata": metadata,
+            }
+        ],
+    }
+    ids = list(
+        client.insert_data(
+            configurations,
+            property_map=property_map,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+
+    client.insert_dataset(
+        pr_hashes=all_do_ids,
+        name=DATASET,
+        authors=AUTHORS,
+        links=LINKS,
+        description=DS_DESC,
+        verbose=True,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
