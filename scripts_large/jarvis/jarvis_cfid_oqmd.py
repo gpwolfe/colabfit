44483eb3d6784f2c1fb52e8165348d87c7c48957
Gregory <setantaontherock1@gmail.com>
2023-08-30 11:54:57 -0400

    Add OQMD CFID script

---
+++ scripts_large/jarvis/jarvis_cfid_oqmd.py
@@ -0,0 +1,238 @@
+"""
+author: gpwolfe
+
+File notes
+----------
+Files have been previously downloaded and unzipped using jarvis-tools to avoid
+having this as a dependency.
+
+Properties key:
+spg = space group
+fund = functional
+slme = spectroscopic limited maximum efficiency
+encut = ecut/energy cutoff
+kpoint_length_unit -> want?
+optb88vdw_total_energy (dft_3d)
+efg = electric field gradient
+mbj_bandgap = band-gap calculated with TBmBJ method
+
+
+For all JARVIS datasets, if for configuration "cartesian=False", use an
+AtomicConfiguration or ase.Atoms object with 'scaled_positions' arg instead of
+'positions'.
+
+Polymer genome keys:
+Original keys (from publication):
+
+Source: VSharma_etal:NatCommun.5.4845(2014)
+Class: organic_polymer_crystal
+Label: Polyimide
+Structure prediction method used: USPEX
+Number of atoms: 32
+Number of atom types: 4
+Atom types: C H O N
+Dielectric constant, electronic: 3.71475E+00
+Dielectric constant, ionic: 1.54812E+00
+Dielectric constant, total: 5.26287E+00
+Band gap at the GGA level (eV): 2.05350E+00
+Band gap at the HSE06 level (eV): 3.30140E+00
+Atomization energy (eV/atom): -6.46371E+00
+Volume of the unit cell (A^3): 2.79303E+02
+
+Keys
+['atoms',
+'desc',
+'form_energy',
+'id',
+'stable',
+'total_energy']
+"""
+
+from argparse import ArgumentParser
+import json
+from numpy import isnan
+from pathlib import Path
+import sys
+
+from colabfit.tools.configuration import AtomicConfiguration
+from colabfit.tools.database import generate_ds_id, load_data, MongoDatabase
+
+from colabfit.tools.property_definitions import potential_energy_pd
+
+
+DATASET_FP = Path("jarvis_json_zips/")
+GLOB = "CFID_OQMD_460k.json"
+DS_NAME = "JARVIS_CFID_OQMD"
+DS_DESC = (
+    "The JARVIS_CFID_OQMD dataset is part of the joint automated repository for "
+    "various integrated simulations (JARVIS) database. This dataset contains "
+    "configurations from the Open Quantum Materials Database, created to hold "
+    "information about the electronic structure and stability of organic materials "
+    "for the purpose of aiding in materials discovery. Calculations were performed "
+    "at the DFT level of theory, using the PAW-PBE functional implemented by VASP. "
+    "JARVIS is a set of tools and collected datasets built to meet current materials "
+    "design challenges."
+)
+
+LINKS = [
+    "https://doi.org/10.1038/npjcompumats.2015.10",
+    "https://jarvis.nist.gov/",
+    "https://ndownloader.figshare.com/files/24981170",
+]
+AUTHORS = [
+    "Scott Kirklin",
+    "James E Saal",
+    "Bryce Meredig",
+    "Alex Thompson",
+    "Jeff W Doak",
+    "Muratahan Aykol",
+    "Stephan Rühl",
+    "Chris Wolverton",
+]
+ELEMENTS = None
+
+
+PROPERTY_MAP = {
+    "potential-energy": [
+        {
+            "energy": {"field": "total_energy", "units": "eV"},
+            "per-atom": {"value": False, "units": None},
+            "_metadata": {
+                "software": {"value": "VASP"},
+                "method": {"value": "DFT-PBE"},
+            },
+        },
+    ],
+    "formation-energy": [
+        {
+            "energy": {"field": "form_energy", "units": "eV"},
+            "per-atom": {"value": False, "units": None},
+            "_metadata": {
+                "software": {"value": "VASP"},
+                "method": {"value": "DFT-PBE"},
+            },
+        },
+    ],
+}
+
+
+with open("formation_energy.json", "r") as f:
+    formation_energy_pd = json.load(f)
+# with open("band_gap.json", "r") as f:
+#     band_gap_pd = json.load(f)
+
+
+def reader(fp):
+    with open(fp, "r") as f:
+        data = json.load(f)
+        data = data[:10000]
+    configs = []
+    for i, row in enumerate(data):
+        atoms = row.pop("atoms")
+        if atoms["cartesian"] is True:
+            config = AtomicConfiguration(
+                positions=atoms["coords"],
+                symbols=atoms["elements"],
+                cell=atoms["lattice_mat"],
+            )
+        else:
+            config = AtomicConfiguration(
+                scaled_positions=atoms["coords"],
+                symbols=atoms["elements"],
+                cell=atoms["lattice_mat"],
+            )
+
+        config.info["name"] = f"{fp.stem}_{i}"
+
+        for key, val in row.items():
+            if type(val) == str and val != "na" and len(val) > 0:
+                config.info[key.replace(" ", "-")] = val
+            elif type(val) == list and len(val) > 0 and any([x != "" for x in val]):
+                config.info[key.replace(" ", "-")] = val
+            elif type(val) == dict and not all([v != "na" for v in val.values()]):
+                config.info[key.replace(" ", "-")] = val
+            elif (type(val) == float or type(val) == int) and not isnan(val):
+                config.info[key.replace(" ", "-")] = val
+            else:
+                pass
+        configs.append(config)
+    return configs
+
+
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    parser.add_argument(
+        "-d",
+        "--db_name",
+        type=str,
+        help="Name of MongoDB database to add dataset to",
+        default="cf-test",
+    )
+    parser.add_argument(
+        "-p",
+        "--nprocs",
+        type=int,
+        help="Number of processors to use for job",
+        default=4,
+    )
+    args = parser.parse_args(argv)
+    client = MongoDatabase(
+        args.db_name, nprocs=args.nprocs, uri=f"mongodb://{args.ip}:27017"
+    )
+
+    ds_id = generate_ds_id()
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB,
+        generator=False,
+    )
+
+    # client.insert_property_definition(free_energy_pd)
+    client.insert_property_definition(formation_energy_pd)
+    client.insert_property_definition(potential_energy_pd)
+
+    ids = list(
+        client.insert_data(
+            configurations=configurations,
+            ds_id=ds_id,
+            co_md_map=CO_METADATA,
+            property_map=PROPERTY_MAP,
+            generator=False,
+            verbose=True,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+
+    client.insert_dataset(
+        ds_id=ds_id,
+        do_hashes=all_do_ids,
+        name=DS_NAME,
+        authors=AUTHORS,
+        links=LINKS,
+        description=DS_DESC,
+        verbose=True,
+    )
+
+
+CO_KEYS = [
+    # "atoms",
+    "desc",
+    # "form_energy",
+    "id",
+    "stable",
+    # "total_energy"
+]
+
+
+CO_METADATA = {key: {"field": key} for key in CO_KEYS}
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
