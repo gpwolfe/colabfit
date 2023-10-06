c464d5cbf76ebe4b410d97ea513a5b2abc6f399a
Gregory <setantaontherock1@gmail.com>
2023-10-06 14:14:37 -0400

    Finish benzene_sag_pnas_2022 script

--- js2_scripts_9_21/js2_scripts/benzene_sag_pnas_2022.py
+++ js2_scripts_9_21/js2_scripts/benzene_sag_pnas_2022.py
@@ -1,245 +1,323 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""
+author: Gregory Wolfe, Alexander Tao
+
+Properties
+----------
+potential energy
+atomic forces
+
+File notes
+----------
+"""
 
 
-from colabfit.tools.database import MongoDatabase, load_data
-from colabfit.tools.property_settings import PropertySettings
-from colabfit.tools.configuration import AtomicConfiguration
+from argparse import ArgumentParser
+from pathlib import Path
+import sys
 
+from ase.io import read
 
-# call database using its name
-# drop_database=True means to start with fresh database
-client = MongoDatabase(
-    "new_data_test_alexander",
-    configuration_type=AtomicConfiguration,
-    nprocs=4,
-    drop_database=True,
+from colabfit.tools.database import generate_ds_id, load_data, MongoDatabase
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    cauchy_stress_pd,
+    potential_energy_pd,
 )
 
-
-# Loads data, specify reader function if not "usual" file format
-configurations = load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/benzene_test_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H"],
-    default_name="benzene_test_QE_PBE_TS",
-    verbose=True,
-    generator=False,
+DATASET_FP = Path(
+    "/persistent/colabfit_raw_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine"
 )
-
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/benzene_train_FPS_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H"],
-    default_name="benzene_train_FPS_QE_PBE_TS",
-    verbose=True,
-    generator=False,
+# comment out below, local path
+# DATASET_FP = Path().cwd().parent / "data/benzene_succinic_acid_glycine"
+DS_NAME = "benzene_succinic_acid_glycine_TS_PNAS_2022"
+DS_DESC_PBE = (
+    "DFT reference energies and forces were calculated using "
+    "Quantum Espresso v6.3. The calculations were performed with "
+    "the semi-local PBE xc functional, Tkatchenko-Scheffler dispersion correction, "
+    "optimised norm-conserving Vanderbilt pseudopotentials, a "
+    "Monkhorst-Pack k-point grid with a maximum spacing of 0.06 x 2π "
+    "A^-1, and a plane-wave energy cut-off of 100 Ry for the wavefunction."
 )
-
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/benzene_val_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H"],
-    default_name="benzene_val_QE_PBE_TS",
-    verbose=True,
-    generator=False,
+DS_DESC_MBD = (
+    "Hybrid-functional DFT energies and forces were calculated using FHI-aims. "
+    "The calculations were performed with the hybrid PBE0 functional and the MBD "
+    "dispersion correCtion (PBE0-MBD), using a Monkhorst-Pack k-point grid with a "
+    'maximum spacing of 0.06 x 2π A^-1, and the standard FHI-AIMS "intermediate" '
+    "basis sets."
 )
+AUTHORS = ["Venkat Kapil", "Edgar A. Engel"]
+LINKS = [
+    "https://doi.org/10.1073/pnas.2111769119",
+    "https://doi.org/10.24435/materialscloud:vp-jf",
+    "https://github.com/venkatkapil24/data_molecular_fluctuations",
+]
+GLOB = "*.xyzdat"
 
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/glycine_test_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "N", "O"],
-    default_name="glycine_test_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
+PI_MD_PBE = {
+    "software": {"value": "Quantum ESPRESSO v6.3"},
+    "method": {"value": "DFT-PBE-TS"},
+    "ecut": {"value": "100 Ry"},
+    "kpoints": {"field": "kpoints"},
+}
+PI_MD_MBD = {
+    "software": {"value": "FHI-aims"},
+    "method": {"value": "DFT-PBE0-MBD"},
+    "ecut": {"value": "100 Ry"},
+    "kpoints": {"field": "kpoints"},
+}
 
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/glycine_train_FPS_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "N", "O"],
-    default_name="glycine_train_FPS_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
-
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/glycine_val_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "N", "O"],
-    default_name="glycine_val_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
-
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/succinic_acid_test_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "O"],
-    default_name="succinic_acid_test_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/succinic_acid_train_FPS_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "O"],
-    default_name="succinic_acid_train_FPS_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
-configurations += load_data(
-    file_path="/large_data/new_raw_datasets_2.0/benzene_succinic_acid_glycine/succinic_acid_val_QE_PBE_TS.xyz",
-    file_format="xyz",
-    name_field=None,
-    elements=["C", "H", "O"],
-    default_name="succinic_acid_val_QE_PBE_TS",
-    verbose=True,
-    generator=False,
-)
-
-
-client.insert_property_definition("/home/ubuntu/notebooks/potential-energy.json")
-client.insert_property_definition("/home/ubuntu/notebooks/atomic-forces.json")
-client.insert_property_definition("/home/ubuntu/notebooks/cauchy-stress.json")
-
-
-property_map = {
+property_map_pbe = {
     "potential-energy": [
         {
             "energy": {"field": "energy", "units": "eV"},
-            "per-atom": {"field": "per-atom", "units": None},
-            # For metadata want: software, method (DFT-XC Functional), basis information, more generic parameters
-            "_metadata": {
-                "software": {"value": "Quantum Espresso v6.3"},
-                "method": {"value": "PBE-TS"},
-                "kpoint": {
-                    "value": "Monkhorst-Pack k-point grid with a maximum spacing of 0.06 × 2π Å−1"
-                },
-                "ecut": {"value": "100 Rydberg"},
-            },
+            "per-atom": {"value": False, "units": None},
+            "_metadata": PI_MD_PBE,
         }
     ],
     "atomic-forces": [
         {
             "forces": {"field": "forces", "units": "eV/Ang"},
-            "_metadata": {
-                "software": {"value": "Quantum Espresso v6.3"},
-                "method": {"value": "PBE-TS"},
-                "kpoint": {
-                    "value": "Monkhorst-Pack k-point grid with a maximum spacing of 0.06 × 2π Å−1"
-                },
-                "ecut": {"value": "100 Rydberg"},
-            },
+            "_metadata": PI_MD_PBE,
         }
     ],
     "cauchy-stress": [
         {
             "stress": {"field": "stress", "units": "eV/Ang^3"},
-            "_metadata": {
-                "software": {"value": "Quantum Espresso v6.3"},
-                "method": {"value": "PBE-TS"},
-                "kpoint": {
-                    "value": "Monkhorst-Pack k-point grid with a maximum spacing of 0.06 × 2π Å−1"
-                },
-                "ecut": {"value": "100 Rydberg"},
-            },
+            "volume-normalized": {"value": False, "units": None},
+            "_metadata": PI_MD_PBE,
+        }
+    ],
+}
+property_map_mbd = {
+    "potential-energy": [
+        {
+            "energy": {"field": "energy", "units": "eV"},
+            "per-atom": {"value": False, "units": None},
+            "_metadata": PI_MD_MBD,
+        }
+    ],
+    "atomic-forces": [
+        {
+            "forces": {"field": "forces", "units": "eV/Ang"},
+            "_metadata": PI_MD_MBD,
+        }
+    ],
+    "cauchy-stress": [
+        {
+            "stress": {"field": "stress", "units": "eV/Ang^3"},
+            "_metadata": PI_MD_MBD,
         }
     ],
 }
 
 
-def tform(c):
-    c.info["per-atom"] = False
+def reader(fp):
+    name = fp.stem
+    configs = read(fp, index=":", format="extxyz")
+    for config in configs:
+        config.info["name"] = name
+        config.info["kpoints"] = "x".join(config.info["kpts"].astype(str))
+    return configs
 
 
-ids = list(
-    client.insert_data(
-        configurations,
-        property_map=property_map,
-        generator=False,
-        transform=tform,
-        verbose=True,
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    parser.add_argument(
+        "-d",
+        "--db_name",
+        type=str,
+        help="Name of MongoDB database to add dataset to",
+        default="cf-test",
     )
-)
+    parser.add_argument(
+        "-p",
+        "--nprocs",
+        type=int,
+        help="Number of processors to use for job",
+        default=4,
+    )
+    args = parser.parse_args(argv)
+    client = MongoDatabase(
+        args.db_name, nprocs=args.nprocs, uri=f"mongodb://{args.ip}:27017"
+    )
 
-all_co_ids, all_pr_ids = list(zip(*ids))
+    client.insert_property_definition(potential_energy_pd)
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(cauchy_stress_pd)
+
+    dss = (
+        # PBE datasets
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_benzene_test",
+            "benzene_test_QE_PBE_TS.xyz",
+            'Benzene test PBE-TS dataset from "Semi-local and hybrid functional '
+            "DFT data for thermalised snapshots of polymorphs of benzene, succinic "
+            'acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_benzene_train",
+            "benzene_train_FPS_QE_PBE_TS.xyz",
+            'Benzene training PBE-TS dataset from "Semi-local and hybrid functional '
+            "DFT data for thermalised snapshots of polymorphs of benzene, succinic "
+            'acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_benzene_validation",
+            "benzene_val_QE_PBE_TS.xyz",
+            'Benzene validation PBE-TS dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_glycine_test",
+            "glycine_test_QE_PBE_TS.xyz",
+            'Glycine test PBE-TS dataset from "Semi-local and hybrid functional '
+            "DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_glycine_train",
+            "glycine_train_FPS_QE_PBE_TS.xyz",
+            'Glycine training PBE-TS dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_glycine_validation",
+            "glycine_val_QE_PBE_TS.xyz",
+            'Glycine validation PBE-TS dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_succinic_acid_test",
+            "succinic_acid_test_QE_PBE_TS.xyz",
+            'Succinic acid test PBE-TS dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_succinic_acid_train",
+            "succinic_acid_train_FPS_QE_PBE_TS.xyz",
+            'Succinic acid training PBE-TS dataset from "Semi-local and '
+            "hybrid functional DFT data for thermalised snapshots of polymorphs "
+            'of benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE_TS_succinic_acid_validation",
+            "succinic_acid_val_QE_PBE_TS.xyz",
+            'Succinic acid validation PBE-TS dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        # MBD datasets
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_benzene_test",
+            "benzene_test_AIMS_PBE0_MBD.xyz",
+            'Benzene test PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_benzene_train",
+            "benzene_train_FPS_AIMS_PBE0_MBD.xyz",
+            'Benzene training PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_benzene_validation",
+            "benzene_val_AIMS_PBE0_MBD.xyz",
+            'Benzene validation PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_glycine_test",
+            "glycine_test_AIMS_PBE0_MBD.xyz",
+            'Glycine test PBE0-MBD dataset from "Semi-local and hybrid functional '
+            "DFT data for thermalised snapshots of polymorphs of benzene, succinic "
+            'acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_glycine_train",
+            "glycine_train_FPS_AIMS_PBE0_MBD.xyz",
+            'Glycine training PBE0-MBD dataset from "Semi-local and hybrid functional '
+            "DFT data for thermalised snapshots of polymorphs of benzene, succinic "
+            'acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_glycine_validation",
+            "glycine_val_AIMS_PBE0_MBD.xyz",
+            'Glycine validation PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_succinic_acid_test",
+            "succinic_acid_test_AIMS_PBE0_MBD.xyz",
+            'Succinic acid test PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of "
+            'benzene, succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_succinic_acid_train",
+            "succinic_acid_train_FPS_AIMS_PBE0_MBD.xyz",
+            'Succinic acid training PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+        (
+            "DFT_polymorphs_PNAS_2022_PBE0_MBD_succinic_acid_validation",
+            "succinic_acid_val_AIMS_PBE0_MBD.xyz",
+            'Succinic acid validation PBE0-MBD dataset from "Semi-local and hybrid '
+            "functional DFT data for thermalised snapshots of polymorphs of benzene, "
+            'succinic acid, and glycine". ' + DS_DESC_PBE,
+        ),
+    )
+
+    for i, (name, reg, desc) in enumerate(dss):
+        ds_id = generate_ds_id()
+        if "PBE" in name:
+            property_map = property_map_pbe
+        else:
+            property_map = property_map_mbd
+
+        configurations = load_data(
+            file_path=DATASET_FP,
+            file_format="folder",
+            name_field="name",
+            elements=None,
+            verbose=True,
+            reader=reader,
+            generator=False,
+            glob_string=reg,
+        )
+
+        ids = list(
+            client.insert_data(
+                configurations,
+                ds_id=ds_id,
+                property_map=property_map,
+                generator=False,
+                verbose=True,
+            )
+        )
+
+        all_co_ids, all_pr_ids = list(zip(*ids))
+        client.insert_dataset(
+            do_hashes=all_pr_ids,
+            ds_id=ds_id,
+            name=name,
+            authors=AUTHORS,
+            links=LINKS,
+            description=desc,
+            verbose=True,
+        )
 
 
-# matches to data CO "name" field
-cs_regexes = {
-    #    '.*':
-    #        'DFT reference energies and forces were calculated using '\
-    #        'Quantum Espresso v6.3. The calculations were per-formed with '\
-    #        'the semi-local PBE exchange-correlation functional, the '\
-    #        'Tkatchenko-Scheffler (TS) disper-sion correction, optimised '\
-    #        'norm-conserving Van-derbilt pseudopotentials,',
-    "benzene_test_QE_PBE_TS": "benzene test datasets.",
-    "benzene_train_FPS_QE_PBE_TS": "benzene datasets for training.",
-    "benzene_val_QE_PBE_TS": "benzene datasets for validation.",
-    "glycine_test_QE_PBE_TS": "glycine test datasets.",
-    "glycine_train_FPS_QE_PBE_TS": "glycine datasets for training.",
-    "glycine_val_QE_PBE_TS": "glycine datasets for validation.",
-    "succinic_acid_test_QE_PBE_TS": "succinic test datasets.",
-    "succinic_acid_train_FPS_QE_PBE_TS": "succinic datasets for training.",
-    "succinic_acid_val_QE_PBE_TS": "succinic datasets for validation.",
-}
-
-cs_names = [
-    "benzene test",
-    "benzene train",
-    "benzene validation",
-    "glycine test",
-    "glycine train",
-    "glycine validation",
-    "succinic test",
-    "succinic train",
-    "succinic validation",
-]
-
-
-cs_ids = []
-
-for i, (regex, desc) in enumerate(cs_regexes.items()):
-    co_ids = client.get_data(
-        "configurations",
-        fields="hash",
-        query={"hash": {"$in": all_co_ids}, "names": {"$regex": regex}},
-        ravel=True,
-    ).tolist()
-
-    print(f"Configuration set {i}", f"({regex}):".rjust(22), f"{len(co_ids)}".rjust(7))
-
-    cs_id = client.insert_configuration_set(co_ids, description=desc, name=cs_names[i])
-
-    cs_ids.append(cs_id)
-
-
-ds_id = client.insert_dataset(
-    cs_ids=cs_ids,
-    do_hashes=all_pr_ids,
-    name="benzene_succinic_acid_glycine_TS_PNAS_2022",
-    authors=["Venkat Kapil", "Edgar A. Engel"],
-    links=[
-        "https://www.pnas.org/doi/10.1073/pnas.2111769119#sec-4",
-        "https://archive.materialscloud.org/record/2021.51",
-        "https://github.com/venkatkapil24/data_molecular_fluctuations",
-    ],
-    description="DFT reference energies and forces were calculated using "
-    "Quantum Espresso v6.3. The calculations were per-formed with "
-    "the semi-local PBE exchange-correlation functional, the "
-    "Tkatchenko-Scheffler (TS) disper-sion correction, optimised "
-    "norm-conserving Van-derbilt pseudopotentials, a "
-    "Monkhorst-Pack k-point grid with a maximum spacing of 0.06 × 2π "
-    "Å−1, and a plane-wave energy cut-off of 100 Rydberg for the wavefunction.",
-    resync=True,
-    verbose=True,
-)
+if __name__ == "__main__":
+    main(sys.argv[1:])
