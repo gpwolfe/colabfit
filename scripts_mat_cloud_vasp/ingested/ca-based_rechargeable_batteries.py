1f3dac97586c70456ae4fb16f4e5b939c10a5ea1
gpwolfe <g.patrick.wolfe@gmail.com>
2024-02-14 16:16:22 -0500

    Add ca-based rechargeable batter script

---
+++ scripts_mat_cloud_vasp/ingested/ca-based_rechargeable_batteries.py
@@ -0,0 +1,436 @@
+"""
+author: Gregory Wolfe
+
+Properties
+----------
+
+Other properties added to metadata
+----------------------------------
+
+File notes
+----------
+Symbols determined by finding a CONTCARini, POSCARini or POSCARn (or similar)
+file. These are explicitly mapped.
+
+excluding structs from files in x0 or x1 folders, as these do not
+reliably have defined atom types
+"""
+
+from argparse import ArgumentParser
+import functools
+import logging
+import re
+import time
+from pathlib import Path
+import sys
+
+from ase.atoms import Atoms
+import pymongo
+
+# from colabfit.tools.configuration import AtomicConfiguration
+from colabfit.tools.database import generate_ds_id, load_data, MongoDatabase
+from colabfit.tools.property_definitions import (
+    atomic_forces_pd,
+    cauchy_stress_pd,
+    potential_energy_pd,
+)
+
+
+DATASET_FP = Path(
+    "data/analysis_of_minerals_as_electrode_materials_for_ca-"
+    "based_rechargeable_batteries"
+)
+DATASET_NAME = "electrode_materials_for_ca-based_rechargeable_batteries"
+LICENSE = "https://creativecommons.org/licenses/by/4.0/"
+
+PUBLICATION = "http://doi.org/10.1038/s41598-019-46002-4"
+DATA_LINK = "https://doi.org/10.24435/materialscloud:3n-e8"
+# OTHER_LINKS = []
+
+AUTHORS = ["M. Elena Arroyo-de Dompablo", "Jose Luis Casals"]
+DATASET_DESC = (
+    'Dataset for "Analysis of minerals as electrode materials for Ca-based '
+    'rechargeable batteries". Includes DFT structures of pyroxenes, garnet '
+    "and carbonates. Dataset was produced to pursue identification of Ca-based "
+    "high specific energy cathode materials."
+)
+ELEMENTS = None
+GLOB_STR = "README.txt"
+
+PI_METADATA = {
+    "software": {"value": "VASP"},
+    "method": {"value": "DFT-PBE"},
+    "input": {"field": "input"},
+}
+
+PROPERTY_MAP = {
+    "potential-energy": [
+        {
+            "energy": {"field": "energy", "units": "eV"},
+            "per-atom": {"value": False, "units": None},
+            "_metadata": PI_METADATA,
+        }
+    ],
+    "atomic-forces": [
+        {
+            "forces": {"field": "forces", "units": "eV/angstrom"},
+            "_metadata": PI_METADATA,
+        },
+    ],
+    "cauchy-stress": [
+        {
+            "stress": {"field": "stress", "units": "kilobar"},
+            "volume-normalized": {"value": False, "units": None},
+            "_metadata": PI_METADATA,
+        }
+    ],
+}
+
+symbol_map = {
+    "camni2o6": ["Ca", "Mn", "C", "O"],
+    "crgarnet": ["Ca", "Cr", "Si", "O"],
+    "icsd27388": ["Ca", "Mn", "Si", "O"],
+    "celdaMn112": ["Ca", "Mn", "Si", "O"],
+    "dolomita": ["Ca", "Mn", "C", "O"],
+    "202248": ["Ca", "Mn", "C", "O"],
+}
+
+# CO_METADATA = {
+#     "enthalpy": {"field": "h", "units": "Ha"},
+#     "zpve": {"field": "zpve", "units": "Ha"},
+# }
+
+CSS = [
+    [
+        f"{DATASET_NAME}_{elem}",
+        {"names": {"$regex": f"{elem}"}},
+        f"{elem.capitalize()} configurations from {DATASET_NAME} dataset",
+    ]
+    for elem in ["piroxene", "garnet", "carbonates"]
+]
+
+
+coord_re = re.compile(
+    r"^\s+(?P<x>\-?\d+\.\d+)\s+(?P<y>\-?\d+\.\d+)\s+(?P<z>\-?\d+\.\d+)\s+(?P<fx>\-?"
+    r"\d+\.\d+)\s+(?P<fy>\-?\d+\.\d+)\s+(?P<fz>\-?\d+\.\d+)"
+)
+param_re = re.compile(
+    r"[\s+]?(?P<param>[A-Z_]+)(\s+)?=(\s+)?(?P<val>-?([\d\w\.\-]+)?\.?)"
+    r"[\s;]?(?P<unit>eV)?\:?"
+)
+IGNORE_PARAMS = [
+    "VRHFIN",
+    "LEXCH",
+    "EATOM",
+    "TITEL",
+    "LULTRA",
+    "IUNSCR",
+    "RPACOR",
+    "POMASS",
+    "RCORE",
+    "RWIGS",
+    "ENMAX",
+    "RCLOC",
+    "LCOR",
+    "LPAW",
+    "EAUG",
+    "DEXC",
+    "RMAX",
+    "RAUG",
+    "RDEP",
+    "RDEPT",
+]
+
+
+def contcar_parser(fp):
+    with open(fp, "r") as f:
+        header = f.readline()
+        for key, val in symbol_map.items():
+            if key in header:
+                symbols = val
+
+        for i in range(4):
+            _ = f.readline()
+        counts = [int(x) for x in f.readline().strip().split()]
+        symbol_arr = []
+        for symbol, count in zip(symbols, counts):
+            symbol_arr.extend([symbol] * count)
+        return symbol_arr
+
+
+def namer(fp):
+    ds_fp_str = "__".join(DATASET_FP.absolute().parts).replace("/", "")
+    name = (
+        "__".join(fp.absolute().parts[:-1])
+        .replace(ds_fp_str + "__", "")
+        .replace("/", "")
+    )
+    return name
+
+
+def convert_stress(keys, stress):
+    stresses = {k: s for k, s in zip(keys, stress)}
+    return [
+        [stresses["xx"], stresses["xy"], stresses["zx"]],
+        [stresses["xy"], stresses["yy"], stresses["yz"]],
+        [stresses["zx"], stresses["yz"], stresses["zz"]],
+    ]
+
+
+def outcar_reader(symbols, fp):
+    with open(fp, "r") as f:
+        configs = []
+        incar = dict()
+        cinput = dict()
+        in_latt = False
+        in_coords = False
+        lattice = []
+        pos = []
+        forces = []
+        stress = None
+        potcars = set()
+        energy = None
+        for line in f:
+            # Prelim handling
+            if line.strip() == "":
+                pass
+
+            # handle lattice
+            elif "direct lattice vectors" in line:
+                in_latt = True
+                lattice = []
+                pass
+            elif in_latt is True:
+                latt = line.strip().replace("-", " -").split()
+                lattice.append([float(x) for x in [latt[0], latt[1], latt[2]]])
+                if len(lattice) == 3:
+                    in_latt = False
+            elif "POTCAR:" in line:
+                potcars.add(" ".join(line.strip().split()[1:]))
+            elif "FREE ENERGIE OF THE ION-ELECTRON SYSTEM" in line:
+                _ = f.readline()
+                _, _, _, _, energy, units = f.readline().strip().split()
+                if len(pos) > 0:
+                    cinput["incar"] = incar
+                    config = Atoms(positions=pos, symbols=symbols, cell=lattice)
+                    config.info["input"] = cinput
+                    config.info["input"]["potcars"] = list(potcars)
+                    # config.info["outcar"] = outcar
+                    if stress is not None:
+                        config.info["stress"] = stress
+                    config.info["forces"] = forces
+                    config.info["energy"] = float(energy)
+                    configs.append(config)
+                    forces = []
+                    stress = None
+                    pos = []
+                    energy = None
+            elif "POSITION" in line:
+                in_coords = True
+                pass
+            elif in_coords is True:
+                if "--------" in line:
+                    pass
+                elif "total drift" in line:
+                    in_coords = False
+                    if energy is not None:
+                        cinput["incar"] = incar
+                        config = Atoms(positions=pos, symbols=symbols, cell=lattice)
+                        config.info["input"] = cinput
+                        config.info["input"]["potcars"] = list(potcars)
+                        # config.info["outcar"] = outcar
+                        config.info["forces"] = forces
+                        if stress is not None:
+                            config.info["stress"] = stress
+                        config.info["energy"] = float(energy)
+                        configs.append(config)
+                        forces = []
+                        stress = None
+                        pos = []
+                        energy = None
+                    else:
+                        pass
+                else:
+                    cmatch = coord_re.search(line)
+                    pos.append(
+                        [float(p) for p in [cmatch["x"], cmatch["y"], cmatch["z"]]]
+                    )
+                    forces.append(
+                        [float(p) for p in [cmatch["fx"], cmatch["fy"], cmatch["fz"]]]
+                    )
+            elif "Direction" in line and "X" in line:
+                stress_keys = [x.lower() for x in line.strip().split()[1:]]
+                # In case keys include "X" instead of "XX", etc.
+                for key in stress_keys:
+                    if len(key) == 1:
+                        stress_keys[stress_keys.index(key)] = key * 2
+
+            elif "in kB" in line:
+                stress = [float(x) for x in line.strip().split()[2:]]
+                stress = convert_stress(stress_keys, stress)
+
+            else:
+                pass
+
+    return configs
+
+
+def get_kpoints(fp):
+    with open(fp, "r") as f:
+        # f.readline() # if skipping first line
+        kpoints = "".join(f.readlines())
+    return kpoints
+
+
+def parse_incar(fp):
+    with open(fp, "r") as f:
+        lines = f.readlines()
+    incar = dict()
+    for line in lines:
+        if "=" in line:
+            keyvals = line.split("=")
+            key = keyvals[0].strip()
+            value = "".join(keyvals[1:]).strip().split("#")[0].strip()
+            incar[key] = value
+    return incar
+
+
+def file_finder(fp, file_glob, count=0):
+    if count > 5:
+        return None
+    elif file_glob in [f.name for f in fp.glob("*")]:
+        return next(fp.glob(file_glob))
+    else:
+        count += 1
+        return file_finder(fp.parent, file_glob, count)
+
+
+def reader(filepath: Path):
+    fps = list(filepath.parent.rglob("OUTCAR"))
+    for fp in fps:
+        if "NEB" not in "".join(fp.parts[-3:]):
+            pass
+        else:
+            name = namer(fp)
+            poscar = next(fp.parent.glob(fp.name.replace("OUTCAR", "CONTCAR")))
+            symbols = contcar_parser(poscar)
+            kpoints_file = file_finder(fp.parent, "KPOINTS")
+            kpoints = get_kpoints(kpoints_file)
+            incar_file = file_finder(fp.parent, "INCAR")
+            incar = parse_incar(incar_file)
+
+            configs = outcar_reader(fp=fp, symbols=symbols)
+            for i, config in enumerate(configs):
+                config.info["name"] = f"{name}__{i}"
+                config.info["input"]["kpoints"] = kpoints
+                config.info["input"]["incar"] = incar
+                yield config
+
+
+MAX_AUTO_RECONNECT_ATTEMPTS = 100
+
+
+def auto_reconnect(mongo_func):
+    """Gracefully handle a reconnection event."""
+
+    @functools.wraps(mongo_func)
+    def wrapper(*args, **kwargs):
+        for attempt in range(MAX_AUTO_RECONNECT_ATTEMPTS):
+            try:
+                return mongo_func(*args, **kwargs)
+            except pymongo.errors.AutoReconnect as e:
+                wait_t = 0.5 * pow(2, attempt)  # exponential back off
+                if wait_t > 1800:
+                    wait_t = 1800  # cap at 1/2 hour
+                logging.warning(
+                    "PyMongo auto-reconnecting... %s. Waiting %.1f seconds.",
+                    str(e),
+                    wait_t,
+                )
+                time.sleep(wait_t)
+
+    return wrapper
+
+
+@auto_reconnect
+def main(argv):
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--ip", type=str, help="IP of host mongod")
+    parser.add_argument(
+        "-d",
+        "--db_name",
+        type=str,
+        help="Name of MongoDB database to add dataset to",
+        default="cf-test",
+    )
+    parser.add_argument(
+        "-p",
+        "--nprocs",
+        type=int,
+        help="Number of processors to use for job",
+        default=4,
+    )
+    parser.add_argument(
+        "-r", "--port", type=int, help="Port to use for MongoDB client", default=27017
+    )
+    args = parser.parse_args(argv)
+    client = MongoDatabase(
+        args.db_name, nprocs=args.nprocs, uri=f"mongodb://{args.ip}:{args.port}"
+    )
+
+    client.insert_property_definition(atomic_forces_pd)
+    client.insert_property_definition(potential_energy_pd)
+    client.insert_property_definition(cauchy_stress_pd)
+
+    ds_id = generate_ds_id()
+
+    configurations = load_data(
+        file_path=DATASET_FP,
+        file_format="folder",
+        name_field="name",
+        elements=ELEMENTS,
+        reader=reader,
+        glob_string=GLOB_STR,
+        generator=False,
+    )
+
+    ids = list(
+        client.insert_data(
+            configurations=configurations,
+            ds_id=ds_id,
+            # co_md_map=CO_METADATA,
+            property_map=PROPERTY_MAP,
+            generator=False,
+            verbose=False,
+        )
+    )
+
+    all_co_ids, all_do_ids = list(zip(*ids))
+
+    cs_ids = []
+    for i, (name, query, desc) in enumerate(CSS):
+        cs_id = client.query_and_insert_configuration_set(
+            co_hashes=all_co_ids,
+            ds_id=ds_id,
+            name=name,
+            description=desc,
+            query=query,
+        )
+
+        cs_ids.append(cs_id)
+
+    client.insert_dataset(
+        do_hashes=all_do_ids,
+        ds_id=ds_id,
+        name=DATASET_NAME,
+        authors=AUTHORS,
+        links=[PUBLICATION, DATA_LINK],  # + OTHER_LINKS,
+        description=DATASET_DESC,
+        verbose=False,
+        cs_ids=cs_ids,  # remove line if no configuration sets to insert
+        data_license=LICENSE,
+    )
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
