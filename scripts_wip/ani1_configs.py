82ad3da1834d8638de285ca3ffa22ad2c26ac07c
Gregory <setantaontherock1@gmail.com>
2024-04-08 11:37:26 -0400

    Update datasets list, ani1 script to smaller batches

--- scripts_wip/ani1_configs.py
+++ scripts_wip/ani1_configs.py
@@ -99,83 +99,60 @@ def auto_reconnect(mongo_func):
     return wrapper
 
 
-def ani_reader(fp, ds_name):
+def ani_reader(fp, ds_name, key_ix):
     with h5py.File(fp) as h5:
         n_heavy = int(fp.stem[-1])
         for key, val in h5.items():
-            for k1, v1 in val.items():
-                coords = np.array(v1["coordinates"])
-                # print(coords[0])
-                coords_he = np.array(v1["coordinatesHE"])
-                energies = np.array(v1["energies"])
-                energies_he = np.array(v1["energiesHE"])
-                smiles = "".join([x.decode() for x in v1["smiles"]])
-                # print(smiles)
-                species = [x.decode() for x in v1["species"]]
-                # print(species)
-                for i, coord in enumerate(coords):
+            # for k1, v1 in val.items():
+            k1 = list(val.keys())[key_ix]
+            v1 = val[k1]
+
+            coords = np.array(v1["coordinates"])
+            # print(coords[0])
+            coords_he = np.array(v1["coordinatesHE"])
+            energies = np.array(v1["energies"])
+            energies_he = np.array(v1["energiesHE"])
+            smiles = "".join([x.decode() for x in v1["smiles"]])
+            # print(smiles)
+            species = [x.decode() for x in v1["species"]]
+            # print(species)
+            for i, coord in enumerate(coords):
+                config = AtomicConfiguration(
+                    positions=coord,
+                    symbols=species,
+                )
+
+                config.info["energy"] = energies[i]
+                config.info["smiles"] = smiles
+                config.info["name"] = (
+                    f"{ds_name}__n_heavy_{n_heavy}__standard_energy__{i}"
+                )
+                yield config
+                if coords_he.shape[0] > i:
+
                     config = AtomicConfiguration(
-                        positions=coord,
+                        positions=coords_he[i],
                         symbols=species,
                     )
-
-                    config.info["energy"] = energies[i]
+                    config.info["energy"] = energies_he[i]
                     config.info["smiles"] = smiles
                     config.info["name"] = (
-                        f"{ds_name}__n_heavy_{n_heavy}__standard_energy__{i}"
+                        f"{ds_name}__n_heavy_{n_heavy}__high_energy__{i}"
                     )
                     yield config
-                    if coords_he.shape[0] > i:
-
-                        config = AtomicConfiguration(
-                            positions=coords_he[i],
-                            symbols=species,
-                        )
-                        config.info["energy"] = energies_he[i]
-                        config.info["smiles"] = smiles
-                        config.info["name"] = (
-                            f"{ds_name}__n_heavy_{n_heavy}__high_energy__{i}"
-                        )
-                        yield config
 
 
 @auto_reconnect
 def read_wrapper(dbname, uri, nprocs, ds_id, ds_name, cs_ids_fp):
     wrap_time = time.time()
-    client = MongoDatabase(dbname, uri=uri, nprocs=nprocs)
+    client = MongoDatabase(dbname, uri=uri, nprocs=32)
     ids = []
     fps = sorted(list(DATASET_FP.rglob(GLOB_STR)))
     today = time.strftime("%Y-%m-%d")
-    partial_read = partial(ani_reader, ds_name=ds_name)
-    for fp in tqdm(fps[-2:]):
+
+    for fp in tqdm(fps[-1:]):
+
         num_heavy = int(fp.stem[-1])
-
-        insert_time = time.time()
-
-        configurations = load_data(
-            file_path=DATASET_FP,
-            file_format="folder",
-            name_field="name",
-            elements=ELEMENTS,
-            reader=partial_read,
-            glob_string=fp.name,
-            generator=True,
-        )
-        ids_batch = list(
-            client.insert_data(
-                configurations=configurations,
-                ds_id=ds_id,
-                co_md_map=CO_METADATA,
-                property_map=PROPERTY_MAP,
-                generator=False,
-                verbose=True,
-            )
-        )
-        ids.extend(ids_batch)
-        new_insert_time = time.time()
-        print(f"Time to insert: {new_insert_time - insert_time}")
-        insert_time = new_insert_time
-        co_ids, do_ids = list(zip(*ids_batch))
         file_ds_name = ds_name.lower().replace("-", "_")
         co_id_file = Path(
             f"{ds_id}_{file_ds_name}_co_ids/"
@@ -189,10 +166,39 @@ def read_wrapper(dbname, uri, nprocs, ds_id, ds_name, cs_ids_fp):
             f"{ds_id}_do_ids_batch_natoms_{num_heavy}.txt"
         )
         do_id_file.parent.mkdir(parents=True, exist_ok=True)
-        with open(co_id_file, "a") as f:
-            f.writelines([f"{id}\n" for id in co_ids])
-        with open(do_id_file, "a") as f:
-            f.writelines([f"{id}\n" for id in do_ids])
+
+        insert_time = time.time()
+        for i_key in range(0, 47932):  # for number 8
+            partial_read = partial(ani_reader, ds_name=ds_name, key_ix=i_key)
+
+            configurations = load_data(
+                file_path=DATASET_FP,
+                file_format="folder",
+                name_field="name",
+                elements=ELEMENTS,
+                reader=partial_read,
+                glob_string=fp.name,
+                generator=True,
+            )
+            ids_batch = list(
+                client.insert_data(
+                    configurations=configurations,
+                    ds_id=ds_id,
+                    co_md_map=CO_METADATA,
+                    property_map=PROPERTY_MAP,
+                    generator=False,
+                    verbose=True,
+                )
+            )
+            ids.extend(ids_batch)
+            new_insert_time = time.time()
+            print(f"Time to insert: {new_insert_time - insert_time}")
+            insert_time = new_insert_time
+            co_ids, do_ids = list(zip(*ids_batch))
+            with open(co_id_file, "a") as f:
+                f.writelines([f"{id}\n" for id in co_ids])
+            with open(do_id_file, "a") as f:
+                f.writelines([f"{id}\n" for id in do_ids])
 
         print(f"Time to read: {time.time() - wrap_time}")
         css = (
@@ -215,7 +221,7 @@ def read_wrapper(dbname, uri, nprocs, ds_id, ds_name, cs_ids_fp):
                 "ANI-1__num_heavy_1__high_energy",  # 0 configs in sets
                 "ANI-1__num_heavy_3__high_energy",
             ]:
-                pass
+                continue
             else:
                 cs_ids.append(
                     client.query_and_insert_configuration_set(
