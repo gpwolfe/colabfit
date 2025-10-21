aa3410435cd9645d5afa8079938a0059ce7ea4c5
gpwolfe <wolfe.gregory.p@gmail.com>
2025-10-21 12:24:18 -0400

    add carbon24 scripts

--- vast_ingest/carbon24/maya_datasets/carbon_X.py
+++ vast_ingest/carbon24/maya_datasets/carbon_X.py
@@ -29,15 +29,14 @@ loader = VastDataLoader(
 # from colabfit.tools.vast.utils.data_processing import generate_ds_id
 
 # generate_ds_id()
-# DS_ll0wuxuhe5g9_0
-loader.config_table = "ndb.colabfit.dev.co_c24enant"
-loader.config_set_table = "ndb.colabfit.dev.cs_c24enant"
-loader.dataset_table = "ndb.colabfit.dev.ds_c24enant"
-loader.co_cs_map_table = "ndb.colabfit.dev.cs_co_map_c24enant"
+loader.config_table = "ndb.colabfit.dev.co_carbonX"
+loader.config_set_table = "ndb.colabfit.dev.cs_carbonX"
+loader.dataset_table = "ndb.colabfit.dev.ds_carbonX"
+loader.co_cs_map_table = "ndb.colabfit.dev.cs_co_map_carbonX"
 
-DATASET_NAME = "Carbon-24_Unique_with_Enantiomorphs"  # noqa: E501
-DATASET_ID = "DS_ll0wuxuhe5g9_0"
-DESCRIPTION = "This dataset is a companion dataset to Carbon-24 Unique, containing enantiomorph pairs discovered within the Carbon-24 dataset. Carbon-24_Unique_with_Enantiomorphs has been cultivated from Carbon-24 (Pickard 2020, doi: 10.24435/materialscloud:2020.0026/v1). Contains 4,330 entries of unique carbon structures, where enantiomorphs are treated as distinct. The metadata column indicates the index of the respective enantiomorph pair, if any, as well as the original id from Carbon-24."  # noqa: E501
+DATASET_NAME = "Carbon_X"  # noqa: E501
+DATASET_ID = "DS_m0z5hh54l9sq_0"
+DESCRIPTION = "This dataset is a companion dataset to Carbon-24 Unique. Carbon X contains 480 carbon structures of duplicates which have the same cell shape and same number of atoms per unit cell (N=6), with different translations (X) of the fractional coordinates. Carbon_X has been cultivated from Carbon-24 (Pickard 2020, doi: 10.24435/materialscloud:2020.0026/v1). Material IDs from the original dataset are included in the metadata as 'original_id'."  # noqa: E501
 PUBLICATION = "https://doi.org/10.48550/arXiv.2509.12178"
 DATA_LINK = None
 OTHER_LINKS = ["https://archive.materialscloud.org/records/ajs8r-a2755"]
@@ -67,7 +66,6 @@ property_map.set_metadata_field(
 for key in [
     "original_id",
     "index",
-    "enantiomorph_pair_index",
 ]:
     property_map.set_metadata_field(key, key, dynamic=True)
 
@@ -81,14 +79,13 @@ def reader(xyz, csv_file):
     for i, atoms in enumerate(iread(xyz, format="extxyz")):
         atoms.info["original_id"] = orig_ids[i]
         atoms.info["index"] = i
-        atoms.info["enantiomorph_pair_index"] = atoms.info["enantiomorph"]
         atoms.info["_name"] = f"{DATASET_NAME}_index_{i}"
         yield AtomicConfiguration.from_ase(atoms)
 
 
 gen = reader(
-    "carbon_24_unique_withenants/carbon_24_unique_withenants_fixed_header.xyz",
-    "carbon_24_unique_withenants/carbon_24_unique_withenants_materialsID.csv",
+    "carbon_X/carbon_X.xyz",
+    "carbon_X/carbon_X_materialsID.csv",
 )
 
 
@@ -98,7 +95,7 @@ dm = DataManager(
     prop_map=PROPERTY_MAP,
     dataset_id=DATASET_ID,
     standardize_energy=False,
-    read_write_batch_size=10000,
+    read_write_batch_size=50000,
 )
 dm.load_co_po_to_vastdb(loader, batching_ingest=True, check_existing=False)
 dm.create_dataset(
--- vast_ingest/carbon24/maya_datasets/carbon_nxl.py
+++ vast_ingest/carbon24/maya_datasets/carbon_nxl.py
@@ -29,18 +29,20 @@ loader = VastDataLoader(
 # from colabfit.tools.vast.utils.data_processing import generate_ds_id
 
 # generate_ds_id()
-# DS_ll0wuxuhe5g9_0
-loader.config_table = "ndb.colabfit.dev.co_c24enant"
-loader.config_set_table = "ndb.colabfit.dev.cs_c24enant"
-loader.dataset_table = "ndb.colabfit.dev.ds_c24enant"
-loader.co_cs_map_table = "ndb.colabfit.dev.cs_co_map_c24enant"
+loader.config_table = "ndb.colabfit.dev.co_carbonNXL"
+loader.config_set_table = "ndb.colabfit.dev.cs_carbonNXL"
+loader.dataset_table = "ndb.colabfit.dev.ds_carbonNXL"
+loader.co_cs_map_table = "ndb.colabfit.dev.cs_co_map_carbonNXL"
 
-DATASET_NAME = "Carbon-24_Unique_with_Enantiomorphs"  # noqa: E501
-DATASET_ID = "DS_ll0wuxuhe5g9_0"
-DESCRIPTION = "This dataset is a companion dataset to Carbon-24 Unique, containing enantiomorph pairs discovered within the Carbon-24 dataset. Carbon-24_Unique_with_Enantiomorphs has been cultivated from Carbon-24 (Pickard 2020, doi: 10.24435/materialscloud:2020.0026/v1). Contains 4,330 entries of unique carbon structures, where enantiomorphs are treated as distinct. The metadata column indicates the index of the respective enantiomorph pair, if any, as well as the original id from Carbon-24."  # noqa: E501
+DATASET_NAME = "Carbon_NXL"  # noqa: E501
+DATASET_ID = "DS_4n459iomhnor_0"
+DESCRIPTION = "This dataset is a companion dataset to Carbon-24 Unique. Carbon NXL is intended for use in training of minimal “overfitting” testing cases. Contains 353 carbon structures of duplicates which have different numbers of atoms per unit cell (N=6—16), different cell shapes L, and different translations X of the fractional coordinates. Carbon_NXL has been cultivated from Carbon-24 (Pickard 2020, doi: 10.24435/materialscloud:2020.0026/v1). Material IDs from the original dataset are included in the metadata as 'original_id'. Please cite Martirossyan et al. (https://arxiv.org/abs/2509.12178) if your work utilizes this dataset."  # noqa: E501
 PUBLICATION = "https://doi.org/10.48550/arXiv.2509.12178"
 DATA_LINK = None
-OTHER_LINKS = ["https://archive.materialscloud.org/records/ajs8r-a2755"]
+OTHER_LINKS = [
+    "https://archive.materialscloud.org/records/ajs8r-a2755",
+    "https://github.com/txie-93/cdvae",
+]
 PUBLICATION_YEAR = "2025"
 
 AUTHORS = [
@@ -67,7 +69,6 @@ property_map.set_metadata_field(
 for key in [
     "original_id",
     "index",
-    "enantiomorph_pair_index",
 ]:
     property_map.set_metadata_field(key, key, dynamic=True)
 
@@ -81,14 +82,13 @@ def reader(xyz, csv_file):
     for i, atoms in enumerate(iread(xyz, format="extxyz")):
         atoms.info["original_id"] = orig_ids[i]
         atoms.info["index"] = i
-        atoms.info["enantiomorph_pair_index"] = atoms.info["enantiomorph"]
         atoms.info["_name"] = f"{DATASET_NAME}_index_{i}"
         yield AtomicConfiguration.from_ase(atoms)
 
 
 gen = reader(
-    "carbon_24_unique_withenants/carbon_24_unique_withenants_fixed_header.xyz",
-    "carbon_24_unique_withenants/carbon_24_unique_withenants_materialsID.csv",
+    "carbon_NXL/carbon_NXL.xyz",
+    "carbon_NXL/carbon_NXL_materialsID.csv",
 )
 
 
@@ -98,7 +98,7 @@ dm = DataManager(
     prop_map=PROPERTY_MAP,
     dataset_id=DATASET_ID,
     standardize_energy=False,
-    read_write_batch_size=10000,
+    read_write_batch_size=50000,
 )
 dm.load_co_po_to_vastdb(loader, batching_ingest=True, check_existing=False)
 dm.create_dataset(
